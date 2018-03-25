d15952f1383f102c767d130401c63508a2df3750
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2018-03-25 15:07:19 +0530

    add weatherReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city, countryCode='lk'){
     const url = `${ROOT_URL}&q=${city},${countryCode}`
     const request = axios.get(url)
+
+    console.log("Request: ",request);
+    
     return {
         type: FETCH_WEATHER,
         payload: request
