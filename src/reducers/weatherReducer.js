c09f9d2c9410833199252aa7a9de26103c22bea2
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2018-03-26 10:01:09 +0530

    add weather list with charts

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,4 +1,11 @@
-export default function(state = null, action){
-    console.log("Action: ", action)
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action){
+    switch (action.type){
+        case FETCH_WEATHER:
+            // return state.push(action.payload.data) // This is wrong - mutating the state
+            return [action.payload.data, ...state] // return a new instance of state
+            // can use state.concat([action.payload.data])
+    }    
     return state
 }
